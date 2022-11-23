6918b64d79bbc2079d1be87851fb9a722600582e
Azer Kazimov <108828542+azerkazimov@users.noreply.github.com>
2022-11-23 14:54:45 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let word =
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.";
+function findStringLength(str) {
+  return str.length;
+}
+
+let result = findStringLength(word);
+console.log(result);
